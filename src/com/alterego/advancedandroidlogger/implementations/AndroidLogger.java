67f5b41a904486f1c13a687ac57fe73105f73bbb
Sasa Sekulic <ja@sasasekulic.info>
2014-02-07 23:49:12 +0100

    added AndroidLogger

---
+++ src/com/alterego/advancedandroidlogger/implementations/AndroidLogger.java
@@ -0,0 +1,138 @@
+package com.alterego.advancedandroidlogger.implementations;
+
+import android.util.Log;
+
+import com.alterego.advancedandroidlogger.interfaces.ILogger;
+
+public class AndroidLogger implements ILogger {
+
+	private int mLoggingLevel = 2;
+	private String mLoggingTag = "LOGGER";
+
+	/**
+	 * Initializes the AndroidLogger with the default tag "LOGGER" and the 
+	 * default debugging level NORMAL (Log.i).
+	 *
+	 * @param tag Logging tag
+	 */
+	public AndroidLogger() {
+		this(null, LoggingLevel.NORMAL);
+	}
+
+	/**
+	 * Initializes the AndroidLogger with a specified tag and the 
+	 * default debugging level NORMAL (Log.i).
+	 *
+	 * @param tag Logging tag
+	 */
+
+	public AndroidLogger(String tag) {
+		this(tag, LoggingLevel.NORMAL);
+	}
+
+	/**
+	 * Initializes the AndroidLogger with a specified tag and a specified logging
+	 * level.
+	 *
+	 * @param tag   Logging tag
+	 * @param level Logging level {@link ILogger}
+	 */
+
+	public AndroidLogger(String tag, LoggingLevel level) {
+		if (!tag.equals(null))
+			mLoggingTag = tag;
+		setLoggingLevel(level);
+	}
+
+
+	@Override
+	public void verbose(String tag, String content) {
+		if (mLoggingLevel == 0)
+			Log.v(tag, content);
+	}
+
+	@Override
+	public void verbose(String content) {
+		verbose(mLoggingTag, content);
+	}
+
+	@Override
+	public void debug(String tag, String content) {
+		if (mLoggingLevel <= 1)
+			Log.d(tag, content);
+	}
+
+	@Override
+	public void debug(String content) {
+		debug(mLoggingTag, content);
+	}
+
+	@Override
+	public void info(String tag, String content) {
+		if (mLoggingLevel <= 2)
+			Log.i(tag, content);
+	}
+
+	@Override
+	public void info(String content) {
+		info(mLoggingTag, content);
+	}
+
+	@Override
+	public void warning(String tag, String content) {
+		Log.w(tag, content);
+	}
+
+	@Override
+	public void warning(String content) {
+		warning(mLoggingTag, content);
+	}
+
+	@Override
+	public void error(String tag, String content) {
+		Log.e(tag, content);
+	}
+
+	@Override
+	public void error(String content) {
+		error(mLoggingTag, content);
+	}
+
+	@Override
+	public void fail(String tag, String content) {
+		Log.wtf(tag, content);
+	}
+
+	@Override
+	public void fail(String content) {
+		fail(mLoggingTag, content);
+	}
+
+	@Override
+	public void setLoggingLevel(LoggingLevel level) {
+
+		if (level == LoggingLevel.VERBOSE)
+			mLoggingLevel = 0;
+		else if (level == LoggingLevel.DEBUG)
+			mLoggingLevel = 1;
+		else if (level == LoggingLevel.NORMAL)
+			mLoggingLevel = 2;
+		else
+			mLoggingLevel  = 3;
+
+	}
+
+	@Override
+	public ILogger getLogger(Object instance) {
+		return this;
+	}
+	
+	protected void setLoggingLevel (int level) {
+		mLoggingLevel = level;
+	}
+	
+	protected int getLoggingLevel() {
+		return mLoggingLevel;
+	}
+
+}
