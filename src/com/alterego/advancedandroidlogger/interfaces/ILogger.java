f1ce78e00d6c3fd67fbbf9ca272946cfb2146b96
Sasa Sekulic <ja@sasasekulic.info>
2014-02-09 18:50:48 +0100

    added JavaDoc and get/setLoggingLevel and getLoggingTag methods

--- src/com/alterego/advancedandroidlogger/interfaces/ILogger.java
+++ src/com/alterego/advancedandroidlogger/interfaces/ILogger.java
@@ -1,7 +1,21 @@
 package com.alterego.advancedandroidlogger.interfaces;
 
+import android.util.Log;
+
+/**
+ * This is the interface for our Android loggers. It has logging methods in two flavours, with tag and without.
+ * The expected behaviour is that the tag may be passed in the constructor, but can also be overridden.
+ * There is also the {@link setLoggingLevel} method that sets the logging level, below which log calls will not
+ * be logged (e.g. if you set logging level to NORMAL, all the ILogger calls to verbose and debug methods will
+ * not result in printed logs). Finally, there's the {@link getLogger} that will come in handy when setting log
+ * levels and loggers for classes/methods.
+ */
 public interface ILogger {
 	
+	/**
+	 * There are four logging levels = VERBOSE (Log.v), DEBUG (Log.d), NORMAL (Log.i) and WARNING (Log.w, Log.e and Log.wtf).
+	 * Default Android and AdvancedAndroidLogger level is NORMAL. For details see  {@link Log}
+	 */
 	public static enum LoggingLevel {
         VERBOSE, DEBUG, NORMAL, WARNING
     }
@@ -43,7 +57,12 @@ public interface ILogger {
      */
     void setLoggingLevel(LoggingLevel level);
     
+    void setLoggingLevel(int level);
+    
+    int getLoggingLevel();
+    
     ILogger getLogger(Object instance);
-
+    
+    String getLoggingTag(Object instance);
 
 }
